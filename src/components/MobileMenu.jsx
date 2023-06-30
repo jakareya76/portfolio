8c19b62f5e6606556abd15ccbc99494dc7d0faf0
jakareya76 <jakareya1306@gmail.com>
2023-06-30 16:38:51 +0600

    create navbar

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+const MobileMenu = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  return (
+    <div className="block relative md:hidden">
+      {/* Menu Icon */}
+      <div
+        className="relative mt-4 right-3 cursor-pointer"
+        onClick={handleToggleMenu}
+      >
+        {toggleMenu ? (
+          <>
+            <div className="w-[24px] h-[2px] my-2 bg-white -top-2 absolute rotate-45 duration-300"></div>
+            <div className="w-[24px] h-[2px] my-2 bg-white -rotate-45 duration-300"></div>
+          </>
+        ) : (
+          <>
+            <div className="w-[24px] h-[2px] my-2 bg-white duration-300"></div>
+            <div className="w-[24px] h-[2px] my-2 bg-white duration-300"></div>
+            <div className="w-[24px] h-[2px] my-2 bg-white duration-300"></div>
+          </>
+        )}
+      </div>
+
+      {/* Menu */}
+      {toggleMenu && (
+        <div className="flex flex-col absolute top-12 right-0 bg-zinc-900 px-5 py-5 gap-2 rounded">
+          <Link
+            href="/"
+            className="text-[18px] uppercase font-[300]"
+            onClick={handleToggleMenu}
+          >
+            Home
+          </Link>
+          <Link
+            href="/work"
+            className="text-[18px] uppercase font-[300]"
+            onClick={handleToggleMenu}
+          >
+            Work
+          </Link>
+          <Link
+            href="/about"
+            className="text-[18px] uppercase font-[300]"
+            onClick={handleToggleMenu}
+          >
+            About
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+export default MobileMenu;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,34 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-6 text-white">
-      <div className=""></div>
-      <div className="flex items-center justify-center gap-5">
-        <Link href="/">Home</Link>
-        <Link href="/work">Work</Link>
-        <Link href="/about">About</Link>
+    <nav className="flex items-center justify-between p-5 text-white">
+      <div className="flex items-center justify-center">
+        <Image
+          src="/assets/logo.png"
+          width={66}
+          height={66}
+          className="object-cover"
+        />
       </div>
+      <div className="hidden md:flex items-center justify-center gap-5">
+        <div className="flex flex-col items-center justify-center">
+          <Link href="/" className="text-[24px] uppercase font-[300]">
+            Home
+          </Link>
+        </div>
+        <Link href="/work" className="text-[24px] uppercase font-[300]">
+          Work
+        </Link>
+        <Link href="/about" className="text-[24px] uppercase font-[300]">
+          About
+        </Link>
+      </div>
+
+      <MobileMenu />
     </nav>
   );
 };
