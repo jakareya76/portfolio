136a02f58cea63e6b496a4b7c9a50166a42f182f
jakareya76 <jakareya1306@gmail.com>
2023-07-01 20:35:27 +0600

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,10 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="pb-5 w-full">
+      <h1 className="text-white text-center font-thin">
+        All Right Reserved © Jakareya
+      </h1>
+    </footer>
+  );
 };
 export default Footer;
