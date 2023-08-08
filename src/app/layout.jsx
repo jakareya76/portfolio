4fae2f082743db1bd153de359dfbbe6dabfc5a90
jakareya76 <jakareya1306@gmail.com>
2023-08-08 17:53:57 +0600

    change meta data

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,9 +6,9 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 
 export const metadata = {
-  title: "Jakareya || Full Stack Developer",
+  title: "Jakareya || Web Developer",
   description:
-    "Welcome to the portfolio website of Jakareya, a talented full stack web developer. Explore a collection of innovative projects, showcasing Jakareya's expertise in front-end and back-end development. With a passion for crafting seamless user experiences and employing the latest technologies, Jakareya delivers dynamic and responsive web solutions. Discover a showcase of skills, creativity, and dedication, reflected in a diverse range of successful projects. Get inspired and collaborate with Jakareya to bring your web development ideas to life. Let's build the future of the web together.",
+    "Welcome to the portfolio website of Jakareya, a talented web developer.",
 };
 
 const workSans = Work_Sans({ subsets: ["latin"] });
