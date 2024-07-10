09728e83bedf8dfdcfa42a560fe348056c2a8a73
jakareya76 <jakareya1306@gmail.com>
2024-07-10 19:16:37 +0600

    make profile image bigger and fix buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         transition={{ duration: 0.5, delay: 0.6 }}
         src="/jakareya.png"
         alt="jakareya"
-        className="max-w-[100px] h-[100px] rounded-full"
+        className="max-w-[120px] h-[120px] rounded-full"
       />
       <motion.h3
         initial={{ y: 10, opacity: 0 }}
@@ -43,46 +43,49 @@ const Hero = () => {
         skilled in creating interactive and visually appealing website
       </motion.p>
       <div className="flex flex-wrap items-center justify-center gap-5">
-        <motion.a
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.9 }}
-          href="#contact"
-          className="flex items-center justify-center px-8 py-3 text-lg font-semibold text-white duration-200 bg-blue-500 rounded font-inter "
-        >
-          Contact Me
-        </motion.a>
-
-        <motion.a
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.9 }}
-          href="/cv.pdf"
-          download
-          className="flex items-center justify-center px-8 py-3 text-lg font-semibold duration-200 bg-white rounded text-zinc-900 font-inter "
-        >
-          Download CV
-        </motion.a>
-        <motion.a
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.9 }}
-          href="https://www.linkedin.com/in/jakareya-ahmed"
-          target="_black"
-          className="flex items-center justify-center px-4  py-[14px] text-lg font-semibold duration-200 bg-white rounded text-zinc-900 font-inter "
-        >
-          <FaLinkedin size={22} />
-        </motion.a>
-        <motion.a
-          initial={{ y: 10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.9 }}
-          href="https://github.com/jakareya76"
-          target="_black"
-          className="flex items-center justify-center px-4  py-[14px] text-lg font-semibold duration-200 bg-white rounded text-zinc-900 font-inter "
-        >
-          <FaGithub size={22} />
-        </motion.a>
+        <div className="flex items-center justify-center gap-5">
+          <motion.a
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.9 }}
+            href="#contact"
+            className="flex items-center justify-center px-8 py-3 text-lg font-semibold text-white duration-200 bg-blue-500 rounded font-inter "
+          >
+            Contact Me
+          </motion.a>
+          <motion.a
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.9 }}
+            href="/cv.pdf"
+            download
+            className="flex items-center justify-center px-8 py-3 text-lg font-semibold duration-200 bg-white rounded text-zinc-900 font-inter "
+          >
+            Download CV
+          </motion.a>
+        </div>
+        <div className="flex items-center justify-center gap-5">
+          <motion.a
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.9 }}
+            href="https://www.linkedin.com/in/jakareya-ahmed"
+            target="_black"
+            className="flex items-center justify-center px-4  py-[14px] text-lg font-semibold duration-200 bg-white rounded text-zinc-900 font-inter "
+          >
+            <FaLinkedin size={22} />
+          </motion.a>
+          <motion.a
+            initial={{ y: 10, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.9 }}
+            href="https://github.com/jakareya76"
+            target="_black"
+            className="flex items-center justify-center px-4  py-[14px] text-lg font-semibold duration-200 bg-white rounded text-zinc-900 font-inter "
+          >
+            <FaGithub size={22} />
+          </motion.a>
+        </div>
       </div>
     </div>
   );
