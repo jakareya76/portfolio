6ac579086c069e65e626555ea62ab6cda3a62780
jakareya76 <jakareya1306@gmail.com>
2024-09-12 16:57:22 +0600

    remove unuse data

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,39 +4,23 @@ import { FaLinkedinIn, FaTwitter, FaFacebook } from "react-icons/fa";
 export const myWork = [
   {
     id: 0,
-    title: "Dine Ease",
     image: "/1.png",
     link: "https://dine-ease-5606f.web.app",
-    gitLink: "https://github.com/jakareya76/dine-ease-frontend",
-    description:
-      "A comprehensive restaurant management platform designed to streamline operations. It features menu management, online reservations, and customer feedback integration, providing a hassle-free dining experience. Built with a modern tech stack including React, Node Js, Express Js Mongodb, Firebase",
   },
   {
     id: 1,
-    title: "headline Hub",
     image: "/2.png",
     link: "https://headline-hub-762d6.web.app",
-    gitLink: "https://github.com/jakareya76/headline-hub-frontend",
-    description:
-      " A dynamic web application that aggregates and curates news articles from various sources. It offers users a seamless experience in discovering trending headlines, with personalized news recommendations and a clean, user-friendly interface built using React, Node Js, Express Js Mongodb, Firebase",
   },
   {
     id: 2,
-    title: "Other Options",
     image: "/3.png",
     link: "https://other-options.web.app",
-    gitLink: "https://github.com/jakareya76/other-options-frontend",
-    description:
-      " A versatile web application template offering various customizable features tailored for different use cases. It serves as a foundation for building responsive and user-friendly web solutions, incorporating the latest frontend technologies like React and Tailwind CSS.",
   },
   {
     id: 3,
-    title: "Genius Cars",
     image: "/4.png",
     link: "https://genius-cars-1c8ed.web.app",
-    gitLink: "https://github.com/jakareya76/genius-cars-frontend",
-    description:
-      "A professional car repair shop website designed to showcase services, manage appointments, and provide customers with a seamless booking experience. The site features detailed service descriptions, customer testimonials, and an intuitive interface, all built with modern web technologies like React and Tailwind CSS for a sleek, responsive design.",
   },
 ];
 
